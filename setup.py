deef02b957dbcb6800a6b483d7e44cd44771ba79
U. Boscolo <uboscolo@gmail.com>
2015-06-29 17:22:30 -0400

    setup stuff

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Evolver',
+    'author': 'uboscolo',
+    'url': 'file://Applications/evolver',
+    'download_url': 'file://Applications/evolver',
+    'author_email': 'uboscolo@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ncl'],
+    'scripts': [],
+    'name': 'ncl'
+}
+
+setup(**config)
